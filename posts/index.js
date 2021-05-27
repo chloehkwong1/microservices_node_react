bb3913d71ee016165ff3a2290c11776098b787fd
Chloe <chloe.wong@madetech.com>
2021-05-27 18:23:10 +0100

    Posts service creation

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { uuid }= require('uuidv4');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = uuid();
+    const { title } = req.body;
+
+    posts[id] = {
+        id, title
+    };
+
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log('Listening on 4000');
+})
